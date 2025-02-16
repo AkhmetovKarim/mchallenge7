fc6d1cabdb472c2e5f1289f8d1a6226ecc90e5a5
Karim <konanfoi@gmail.com>
2025-02-16 16:19:24 -0500

    Task 3 - Customer Loyalty Discount.

--- cc_7.js
+++ cc_7.js
@@ -1,4 +1,4 @@
-//Task 7 
+//Task 1
 function calculateInvoice(subtotal, taxRate, discount) { //write a function that calc final invoice
     let taxAmount = subtotal * taxRate; //find taxAAmount
     let total = (subtotal + taxAmount) - discount; //find total invoice
@@ -7,10 +7,20 @@ function calculateInvoice(subtotal, taxRate, discount) { //write a function that
 console.log(calculateInvoice(100, 0.08, 5)); //logs
 console.log(calculateInvoice(500, 0.1, 20));
 
-//Tssk 8
+//Task 2
 let calculateHourlyWage = function(salary, hoursPerWeek) { //declare a function
 return `Hourly Wage: $${(salary / (hoursPerWeek * 52)).toFixed(2)}`;
 };
     
 console.log(calculateHourlyWage(52000, 40)); //logs
 console.log(calculateHourlyWage(75000, 35));
+
+//Task 3
+
+const calculateLoyaltyDiscount = (amount, years) => { //write an arrow function that applies discount
+    let discountRate = years >= 5 ? 0.15 : years >= 3 ? 0.10 : 0.05;
+    let discountedPrice = amount - (amount * discountRate);
+    return `Discounted Price: $${discountedPrice.toFixed(2)}`;
+};
+console.log(calculateLoyaltyDiscount(100, 6)); //logs, text cases
+console.log(calculateLoyaltyDiscount(200, 2));
