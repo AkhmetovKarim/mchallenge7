be1dd92cff9ce6fd5a31d806e68cbc0af09a13d4
Karim <konanfoi@gmail.com>
2025-02-16 16:08:39 -0500

    Task 2 - Employee Hourly Wage Calculation.

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,11 @@ function calculateInvoice(subtotal, taxRate, discount) { //write a function that
 }
 console.log(calculateInvoice(100, 0.08, 5)); //logs
 console.log(calculateInvoice(500, 0.1, 20));
+
+//Tssk 8
+let calculateHourlyWage = function(salary, hoursPerWeek) { //declare a function
+return `Hourly Wage: $${(salary / (hoursPerWeek * 52)).toFixed(2)}`;
+};
+    
+console.log(calculateHourlyWage(52000, 40)); //logs
+console.log(calculateHourlyWage(75000, 35));
